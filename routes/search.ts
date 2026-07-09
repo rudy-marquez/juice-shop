5b5fa39243dc7dd3dc6b7492fd6f877659ac1ecd
Rudy <marquezrudy@hotmail.com>
2026-07-09 16:44:10 -0700

    Checkmarx Bot - Auto Pull Request

--- routes/search.ts
+++ routes/search.ts
@@ -6,6 +6,7 @@
 import models = require('../models/index')
 import { type Request, type Response, type NextFunction } from 'express'
 import { UserModel } from '../models/user'
+import { QueryTypes } from 'sequelize'
 
 import * as utils from '../lib/utils'
 const challengeUtils = require('../lib/challengeUtils')
@@ -20,8 +21,8 @@ module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
-      .then(([products]: any) => {
+    models.sequelize.query('SELECT * FROM Products WHERE ((name LIKE :searchCriteria OR description LIKE :searchCriteria) AND deletedAt IS NULL) ORDER BY name', { replacements: { searchCriteria: `%${criteria}%` }, type: QueryTypes.SELECT }) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+      .then((products: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
           let solved = true
